4d013925c4bbee5503852bb55ad2afedae70d098
jordy sabimana <jord.sab@gmail.com>
2024-03-28 19:18:26 +0100

    movie-land

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+import SearchIcon from "./search.svg";
+import MovieCard from "./MovieCard";
 
-function App() {
+const API_URL = "http://www.omdbapi.com?apikey=3ad584cf";
+
+const movie1 = {
+  Title: "Batman Begins",
+  Year: "2005",
+  imdbID: "tt0372784",
+  Type: "movie",
+  Poster:
+    "https://m.media-amazon.com/images/M/MV5BOTY4YjI2N2MtYmFlMC00ZjcyLTg3YjEtMDQyM2ZjYzQ5YWFkXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
+};
+
+const App = () => {
+  const [movies, setMovies] = useState([]); // Movie state
+  const [searchTerm, setSearchTerm] = useState(""); // Search state
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+
+    setMovies(data.Search);
+  };
+  useEffect(() => {
+    searchMovies("Black panther");
+  }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <h1>Movie Land</h1>
+
+      <div className="search">
+        <input
+          placeholder="Search for pictures"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => searchMovies(searchTerm)}
+        />
+      </div>
+
+      {movies?.length > 0 ? ( // If movies array is > 0, populate the movie components
+        <div className="container">
+          {movies.map(
+            (
+              movie // We looping through the movies array from the api
+            ) => (
+              <MovieCard movie={movie} /> // Each movie is then passed as a prop to the component
+            )
+          )}
+        </div>
+      ) : (
+        // Or else
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
